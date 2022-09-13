55574da5d5e51f63bb53b8431137829ffd7d2059
tehilla <tehilla.sharbaf@carmel6000.amitnet.org>
2022-09-13 13:02:30 +0300

    .

--- middlewares/track-users-middleware.js
+++ middlewares/track-users-middleware.js
@@ -2,15 +2,6 @@
 
 function TrackUserMiddleware(req, res, next) {
 
-    const data = {};
-
-    data.timeStamp = new Date();
-    data.userAgent = req.get('User-Agent');
-    data.remoteIp = req.connection.remoteAddress;
-    data.url = req.originalUrl;
-
-    console.log('data: ', data);
-
-    next();
+ 
 }
 module.exports = TrackUserMiddleware;
