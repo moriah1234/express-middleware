6f45cb334a8ecc3c7b70f0b761919d5f14f475d5
dinamashmush <dina.Mashmush@carmel6000.amitnet.org>
2023-01-11 15:58:08 +0200

    .

--- middlewares/redirectIfBLockedMiddleware.js
+++ middlewares/redirectIfBLockedMiddleware.js
@@ -1,6 +1,5 @@
 function redirectIfBlockedMiddleware(req, res, next) {
 
-    console.log('res.locals.isBlocked: ', res.locals.isBlocked);
     if (res.locals.isBlocked) {
         res.redirect('/error')
     }
--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,22 +1,21 @@
 function validationMiddleware(req, res, next) {
 
     if (
-        typeof req.body.username === 'string' &&
         req.body.username &&
-        typeof req.body.password === 'string' &&
+        typeof req.body.username === 'string' &&
         req.body.password &&
-        typeof req.body.mail === 'string' &&
+        typeof req.body.password === 'string' &&
         req.body.mail &&
+        typeof req.body.mail === 'string' &&
         req.body.mail.includes('@') &&
         !isNaN(Number(req.body.phonenumber)) &&
         req.body.phonenumber.length === 10 &&
-        !res.locals.isBlocked //to check taht the user was not already denied (by ip)
+        !res.locals.isBlocked //to check that the user was not already denied (by ip)
     ) {
         console.log('validation good');
         res.locals.isBlocked = false;
     } else {
         console.log('validation bad');
-
         res.locals.isBlocked = true;
     };
 
